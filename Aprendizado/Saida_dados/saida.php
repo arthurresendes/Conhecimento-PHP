abfbc9d2860f1590f006e1520cbfbcad1414391d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-03-28 12:03:56 -0300

    Saida de dados

---
+++ Aprendizado/Saida_dados/saida.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saída de dados</title>
+</head>
+<body>
+    <?php
+        echo 'Ola mundo com echo<br>';
+        print 'Ola mundo com print<br>';
+    ?>
+</body>
+</html>
