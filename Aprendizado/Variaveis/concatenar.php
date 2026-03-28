4902cf85001379eadc5f070e3e8517c74f7f756a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-03-28 12:14:11 -0300

    Concatenação

---
+++ Aprendizado/Variaveis/concatenar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concatenando</title>
+</head>
+<body>
+    <?php
+        $nome = "Josias";
+        $idade = 75;
+
+        echo "Seu nome é: " . $nome . " e você tem " . $idade . " anos<br>";
+    ?>
+</body>
+</html>
