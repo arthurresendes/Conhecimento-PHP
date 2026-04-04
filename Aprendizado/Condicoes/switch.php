9762a546cb0fe97bec9e5e4155b3fa73fdd8764c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-04-04 14:11:21 -0300

    Switch

---
+++ Aprendizado/Condicoes/switch.php
@@ -0,0 +1,16 @@
+<?php
+    $opcao = 2;
+    switch ($opcao) {
+        case 1:
+            echo '<p>Opção 1</p>';
+            break;
+        case 2:
+            echo '<p>Opção 2</p>';
+            break;
+        default:
+            echo '<p>Opção invalida</p>';
+            break;
+    }
+
+
+?>
