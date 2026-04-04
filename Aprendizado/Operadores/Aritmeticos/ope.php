061f4c31f8cbe899eb0ca24024f7217314298ed7
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-04-04 14:18:38 -0300

    Operadores aritmeticos

---
+++ Aprendizado/Operadores/Aritmeticos/ope.php
@@ -0,0 +1,14 @@
+<?php
+    $num1 = 10;
+    $num2  = 2.25;
+
+    echo 'Soma: ' . $num1 + $num2;
+    echo '<br/>';
+    echo 'Subtração: ' . $num1 - $num2;
+    echo '<br/>';
+    echo 'Multiplicação: ' . $num1 * $num2;
+    echo '<br/>';
+    echo 'Divisão: ' . round($num1 / $num2,2);
+    echo '<br/>';
+
+?>
