14dfb599d916f14e9c1134ed9566cf1660f2931b
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-04-04 14:21:53 -0300

    Incremento

---
+++ Aprendizado/Operadores/Incremento/incre.php
@@ -0,0 +1,20 @@
+<?php
+    $num = 10;
+    echo $num;
+    echo '<br/>';
+    ++$num;
+    echo $num;
+    echo '<br/>';
+    
+    $num++;
+    echo $num;
+    echo '<br/>';
+
+    --$num;
+    echo $num;
+    echo '<br/>';
+    $num--;
+    echo $num;
+    echo '<br/>';
+
+?>
