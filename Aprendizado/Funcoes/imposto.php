7d6aa3a835e16ada2e9fecb7df3049f339507d11
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-04-04 14:36:01 -0300

    Calculo imposto com função

--- Aprendizado/Funcoes/imposto.php
+++ Aprendizado/Funcoes/imposto.php
@@ -5,16 +5,16 @@
             echo 'Insento';
         }else if($valor <= 2828.65){
             $imposto = $valor*0.075;
-            echo 'Tera que pagar de imposto nivel 1: ' . round($imposto,2);
+            echo 'Tera que pagar de imposto nivel 1: ' . floor($imposto,2);
         }else if($valor <= 3751.05){
             $imposto = $valor*0.15;
-            echo 'Tera que pagar de imposto nivel 2: ' . round($imposto,2);
+            echo 'Tera que pagar de imposto nivel 2: ' . floor($imposto,2);
         }else if($valor <= 4664.68){
             $imposto = $valor*0.225;
-            echo 'Tera que pagar de imposto nivel 3: ' . round($imposto,2);
+            echo 'Tera que pagar de imposto nivel 3: ' . floor($imposto,2);
         }else{
             $imposto = $valor*0.275;
-            echo 'Tera que pagar de imposto nivel 4: ' . round($imposto,2);
+            echo 'Tera que pagar de imposto nivel 4: ' . floor($imposto,2);
         }
     }
 
---
+++ Aprendizado/Funcoes/matematica.php
@@ -0,0 +1,17 @@
+<?php
+    $num = 10.10243254;
+    $raiz = 9;
+
+    echo ceil($num); // Arredonda para cima
+    echo "<br/>";
+    echo floor($num); // Arredonda para baixo
+    echo "<br/>";
+    echo round($num); // Se for acima ou igual de .5 arredodnda para cima e se for .4 ou abaixo arredonda para baixo
+    echo "<br/>";
+    echo rand(); // Numero aleatori
+    echo "<br/>";
+    echo sqrt($raiz); // Raiz quadrada
+    echo "<br/>";
+
+
+?>
