1a5d7e738e2dffbd1aef92bc3298e427b0868d7f
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-04-11 11:38:07 -0300

    Dates in php

--- Aprendizado/Funcoes/datas.php
+++ Aprendizado/Funcoes/datas.php
@@ -3,6 +3,13 @@
 
     echo $data_atual;
     echo '<br/>';
+    echo date_default_timezone_get();
+    date_default_timezone_set('America/Sao_Paulo');
+    echo '<br/>';
+    echo date_default_timezone_get();
+    echo '<br/>';
+    echo date('d/m/Y H:i')
+    
 
 
 ?>
