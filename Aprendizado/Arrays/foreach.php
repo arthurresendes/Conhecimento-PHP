c7c7c6e80154f2bd5aa73313920011fb66dfface
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-04-11 11:59:15 -0300

    Foreach in arrays

---
+++ Aprendizado/Arrays/foreach.php
@@ -0,0 +1,9 @@
+<?php
+    $lista_nomes = ['Arthur' => 20, 'Jose' => 19];
+
+    foreach($lista_nomes as $nomes => $idade){
+        echo 'Nome: ' . $nomes . ' Idade: ' . $idade;
+        echo '<br/>';
+    }
+
+?>
